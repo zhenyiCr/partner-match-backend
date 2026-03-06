57634e8d31ed709be95252234e9b9d06a74fb535
真意 <15702225+zhen_yicr@user.noreply.gitee.com>
2026-03-06 13:56:02 +0800

    初始化

---
+++ src/test/java/com/yupi/usercenter/service/impl/UserServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.yupi.usercenter.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void searchUserByTags() {
+    }
+}
